ce5a58fdbc254e31fb0124cfb93bb5b1bbea6f7f
borzenx <114352015+borzenx@users.noreply.github.com>
2024-02-08 18:26:29 +0100

    d

--- app/website/page.tsx
+++ app/website/page.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 
 const Website = () => {
     const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
     const searchParams = useSearchParams();
     const website = searchParams.get('q');
 
@@ -16,8 +17,8 @@ const Website = () => {
                 }
                 const jsonData = await response.json();
                 setData(jsonData);
-            } catch (error) {
-                console.log(error);
+            } catch (error: any) {
+                setError(error.message);
             }
         };
 
@@ -26,13 +27,15 @@ const Website = () => {
         }
     }, [website]);
 
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
 
     return (
-        <>
         <div>
-            <h1>{data}</h1>
+            {data ? <h1>{data}</h1> : <p>Loading...</p>}
         </div>
-        </>
     );
-}
+};
+
 export default Website;
