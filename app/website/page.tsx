6ccbf107b36a78bc6911077ac02f3a60fc63c93d
borzenx <139754558+hazle-pl@users.noreply.github.com>
2024-02-08 18:13:03 +0100

    d

---
+++ app/website/page.tsx
@@ -0,0 +1,46 @@
+'use client'
+import { useSearchParams } from 'next/navigation';
+import React, { useState, useEffect } from 'react';
+
+export default function SearchPage() {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const searchParams = useSearchParams();
+    const website = searchParams.get('q');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`https://data.similarweb.com/api/v1/data?domain=${website}`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data');
+                }
+                const jsonData = await response.json();
+                setData(jsonData);
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+
+        if (website) {
+            fetchData();
+        }
+    }, [website]);
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    return (
+        <>
+            {data ? (
+                <div>
+                    <h1>{data}</h1>
+                    {/* Render other data fields as needed */}
+                </div>
+            ) : (
+                <div>Loading...</div>
+            )}
+        </>
+    );
+}
