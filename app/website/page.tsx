2eb47ff4167ad8b34b85d8773abdaa1674f743de
borzenx <114352015+borzenx@users.noreply.github.com>
2024-02-08 18:32:07 +0100

    fds

--- app/website/page.tsx
+++ app/website/page.tsx
@@ -7,32 +7,28 @@ const Website = () => {
     const searchParams = useSearchParams();
     const website = searchParams.get('q');
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`https://data.similarweb.com/api/v1/data?domain=${website}`);
-                if (!response.ok) {
-                    throw new Error('Failed to fetch data');
-                }
-                const jsonData = await response.json();
-                setData(jsonData);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch(`https://data.similarweb.com/api/v1/data?domain=${website}`);
+    //             if (!response.ok) {
+    //                 throw new Error('Failed to fetch data');
+    //             }
+    //             const jsonData = await response.json();
+    //             setData(jsonData);
+    //         } catch (error) {
+    //             console.error('Error fetching data:', error);
+    //         }
+    //     };
 
-        if (website) {
-            fetchData();
-        }
-    }, [website]);
+    //     if (website) {
+    //         fetchData();
+    //     }
+    // }, [website]);
 
     return (
         <div>
-            {data ? (
-                <h1>Data: {JSON.stringify(data)}</h1>
-            ) : (
-                <p>Loading...</p>
-            )}
+            {website}
         </div>
     );
 };
